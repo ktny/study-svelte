ddf3e59d2a681ddc57550d63070527183a8c8d6e
ktny <katsunori.t10@gmail.com>
2022-11-03 20:06:16 +0900

    elizabot

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,108 @@
 <script>
-  let numbers = [1,2,3,4];
+  import Eliza from "elizabot";
+  import { beforeUpdate, afterUpdate } from "svelte";
 
-  function addNumber() {
-    // numbers.push(numbers.length + 1);
-    // numbers = numbers;
-    numbers = [...numbers, numbers.length + 1];
+  let div;
+  let autoscroll;
+  
+  beforeUpdate(() => {
+    autoscroll = div && (div.offsetHeight + div.scrollTop) > (div.scrollHeight - 20);
+  });
+
+  afterUpdate(() => {
+    if (autoscroll) div.scrollTo(0, div.scrollHeight);
+  });
+
+  const eliza = new Eliza();
+
+  let comments = [
+    {author: "eliza", text: eliza.getInitial(), placeholder: false}
+  ];
+
+  function handleKeydown(event) {
+    if (event.key === "Enter") {
+      const text = event.target.value;
+      if (!text) return;
+
+      comments = comments.concat({
+        author: "user",
+        text,
+        placeholder: false,
+      });
+
+      event.target.value = "";
+
+      const reply = eliza.transform(text);
+
+      setTimeout(() => {
+        comments = comments.concat({
+          author: "eliza",
+          text: "...",
+          placeholder: true
+        });
+
+        setTimeout(() => {
+          comments = comments.filter(comment => !comment.placeholder).concat({
+            author: "eliza",
+            text: reply,
+            placeholder: false,
+          });
+        }, 500 + Math.random() * 500);
+      }, 200 + Math.random() * 200);
+    }
   }
-
-  $: sum = numbers.reduce((t, n) => t + n, 0);
-
-  // function incrementCount() {
-  //   count += 1;
-  // }
 </script>
 
-<p>{numbers.join(" + ")} = {sum}</p>
+<div class="chat">
+  <h1>Eliza</h1>
 
-<button on:click={addNumber}>Add a number</button>
+  <div class="scrollable">
+    {#each comments as comment}
+      <article class={comment.author}>
+        <span>{comment.text}</span>
+      </article>
+    {/each}
+  </div>
+</div>
 
-<!-- <button on:click={incrementCount}>
-  Clicked {count} {count === 1 ? "time" : "times"}
-</button>
+  <input on:keydown={handleKeydown}>
+<style>
+	.chat {
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+		max-width: 320px;
+	}
 
-<p>{count} doubled is {doubled}</p> -->
+	.scrollable {
+		flex: 1 1 auto;
+		border-top: 1px solid #eee;
+		margin: 0 0 0.5em 0;
+		overflow-y: auto;
+	}
+
+	article {
+		margin: 0.5em 0;
+	}
+
+	.user {
+		text-align: right;
+	}
+
+	span {
+		padding: 0.5em 1em;
+		display: inline-block;
+	}
+
+	.eliza span {
+		background-color: #eee;
+		border-radius: 1em 1em 1em 0;
+	}
+
+	.user span {
+		background-color: #0074D9;
+		color: white;
+		border-radius: 1em 1em 0 1em;
+		word-break: break-all;
+	}
+</style>
